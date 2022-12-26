44823229c1ac12243b68b6e547c3deffcf2c7aca
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2022-12-26 23:14:16 +0530

    upgraded encryption lib dependencies

--- libraries/encryption/jest.config.js
+++ libraries/encryption/jest.config.js
@@ -1,16 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 const commonConfig = {
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json",
-    },
-  },
   moduleFileExtensions: ["ts", "js"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
   collectCoverageFrom: ["src/**/*.ts"],
   testMatch: ["**/test/**/*.test.(ts|js)", "**/src/**/*.test.(ts|js)"],
-  testTimeout: 5000,
 };
 
 module.exports = {
