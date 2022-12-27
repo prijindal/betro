335c59aa661b15e1d1ca3a0d1a8a205c01d18519
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2022-12-27 12:33:26 +0530

    fixed base url support for default

--- services/ui/src/constants.ts
+++ services/ui/src/constants.ts
@@ -1,4 +1,4 @@
 export const API_HOST: string =
     (import.meta.env.VITE_API_HOST as string | undefined) ||
     window.localStorage.getItem("VITE_API_HOST") ||
-    "http://localhost:4000";
+    window.location.origin;
