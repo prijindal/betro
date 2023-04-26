2e73095d628afa8fa2d8f67248c6677e7231600c
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2023-04-26 21:34:17 +0530

    updated packages

--- services/ui/vite.config.ts
+++ services/ui/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
