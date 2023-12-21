47cc73ed945637ba13a2f5bd3c47a626de83b901
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2023-12-21 13:24:50 +0530

    Removed mantine

--- services/ui/src/components/AppDrawer/MobileAppDrawer.tsx
+++ services/ui/src/components/AppDrawer/MobileAppDrawer.tsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { Drawer } from "@mantine/core";
 import AppDrawer from "../AppDrawer";
 
 const MobileAppDrawer: React.FunctionComponent<{
-    includeRouting: boolean;
-    open: boolean;
-    setOpen: (b: boolean) => void;
+  includeRouting: boolean;
+  open: boolean;
+  setOpen: (b: boolean) => void;
 }> = (props) => {
-    const { includeRouting, open, setOpen } = props;
-    return (
-        <Drawer onClose={() => setOpen(false)} opened={open} withCloseButton={false}>
-            <AppDrawer includeRouting={includeRouting} />
-        </Drawer>
-    );
+  const { includeRouting, open, setOpen } = props;
+  return (
+    <>
+      <div
+        className={`${
+          open ? "absolute" : "hidden"
+        } h-full w-full z-20 bg-black/20 cursor-pointer`}
+        onClick={() => setOpen(false)}
+      />
+      <div
+        className={`${
+          open ? "absolute" : "hidden"
+        } z-20 bg-white shadow-subtle h-full`}
+      >
+        <AppDrawer includeRouting={includeRouting} />
+      </div>
+    </>
+  );
 };
 
 export default MobileAppDrawer;
